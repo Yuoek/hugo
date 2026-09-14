259db685cbaa2b7a9fd3016e4743f6ff3d574169
yuoek <2055048783@qq.com>
2026-09-15 07:54:10 +0800

    to 978

--- public/science/computer/algorithm/leetcode/0100-0199/0151.reverse-words-in-a-string/Solution2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        return " ".join(reversed(s.split()))
-
-if __name__ == "__main__":
-    sol = Solution()
-    print(repr(sol.reverseWords("the sky is blue")))
-    print(repr(sol.reverseWords("  hello world  ")))
-    print(repr(sol.reverseWords("a good   example")))
